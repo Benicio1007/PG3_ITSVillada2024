55395105abd610ffe319cec13285b5e7a0200007
Benicio1007 <beniciomanztti@gmail.com>
2024-04-12 10:33:13 -0300

    ejercicio1

---
+++ PROG III/3_Manejo_de_exceptiones_con_python/ejercicio1.py
@@ -0,0 +1,20 @@
+def sumar_numeros():
+    while True:
+        try:
+            num1 = int(input("Ingrese el primer número entero \n >>> "))
+            num2 = int(input("Ingrese el segundo número entero \n >>> "))
+            
+            suma = num1 + num2
+            
+            print("La suma de", num1, "y", num2, "es \n >>>", suma)
+            
+            continuar = input("¿Quiere seguir sumando valores? (s/n) \n >>> ")
+            
+           
+            if continuar.lower() != 's':
+                break
+        except ValueError:
+            print("Error: Tiene ingresar números enteros. ")
+
+
+sumar_numeros()
